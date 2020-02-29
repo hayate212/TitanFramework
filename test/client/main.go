0d008275179cc17998a166fbd490be7b6631588a
hayate212 <hayate.JvN@gmail.com>
2020-02-29 16:40:36 +0900

    changed to use seviper

--- test/client/main.go
+++ test/client/main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"net"
 	"time"
+
+	"github.com/hayate212/seviper"
 )
 
 func main() {
-	conn, _ := net.Dial("tcp", "localhost:9999")
-	buff := []byte{00, 00, 00, 00}
+	conn, err := net.Dial("tcp", "localhost:9999")
+	if err != nil {
+		return
+	}
+	c := 0
 	for {
-		conn.Write(buff)
-		buff[0]++
+		w := seviper.NewWriter()
+		w.Write("test")
+		w.Write(c)
+		w.Write(c * c)
+		_, err := conn.Write(w.Bytes)
+		if err != nil {
+			return
+		}
+		c += 16
 		time.Sleep(time.Second * 1)
 	}
 }
